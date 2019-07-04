b0cb1eca71eed7f7ccb34beca502b1e8309282d0
noor <noor.elshafie@gmail.com>
2019-07-05 01:05:23 +0200

    Fragment creation edited_2_appBar

--- app/src/main/java/com/example/lessononemultiscreen/MainActivity.java
+++ app/src/main/java/com/example/lessononemultiscreen/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lessononemultiscreen;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,8 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ViewPager viewPager=findViewById(R.id.viewpager);
-        MiwokViewPagerAdapter adapter=new MiwokViewPagerAdapter(getSupportFragmentManager());
+        MiwokViewPagerAdapter adapter=new MiwokViewPagerAdapter(this,getSupportFragmentManager());
         viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
 
     }
--- app/src/main/java/com/example/lessononemultiscreen/MiwokViewPagerAdapter.java
+++ app/src/main/java/com/example/lessononemultiscreen/MiwokViewPagerAdapter.java
@@ -1,13 +1,35 @@
 package com.example.lessononemultiscreen;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class MiwokViewPagerAdapter extends FragmentPagerAdapter {
+    private Context mContext;
 
     public MiwokViewPagerAdapter(FragmentManager fm) {
         super(fm);
+    }
+
+    public MiwokViewPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 
     @Override
